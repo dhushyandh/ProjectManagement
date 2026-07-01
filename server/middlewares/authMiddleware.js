b091d020ebae5973e34ba5849b8067afaf6fc331
Your Name <you@example.com>
2026-07-01 22:29:52 +0530

    Frontend & Backend Completed

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,15 +1,35 @@
-export const protect = async (req,res,next) => {
-    try {
-        const {userId} = await req.auth()
+import { prisma } from "../config/prisma.js";
+import { clerkClient } from "@clerk/express";
 
-        if(!userId){
-            return res.status(401).json({message: "Unauthorized"})
-        }
-        return next()
+export const protect = async (req, res, next) => {
+  try {
+    const { userId } = await req.auth();
 
-    } catch (error) {
-        console.error(error)
-        return res.status(500).json({message: "Internal Server Error"})
+    if (!userId) {
+      return res.status(401).json({ message: "Unauthorized" });
     }
-    
-}
+
+    const existing = await prisma.user.findUnique({
+      where: { id: userId }
+    });
+
+    if (!existing) {
+      const user = await clerkClient.users.getUser(userId);
+
+      await prisma.user.create({
+        data: {
+          id: user.id,
+          email: user.emailAddresses[0].emailAddress,
+          name: `${user.firstName ?? ""} ${user.lastName ?? ""}`,
+          image: user.imageUrl ?? "",
+        }
+      });
+    }
+
+    next();
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
